d315ad3ad6f5b048f515c723bbbb59292d43c2fd
Flagelo6482 <frankln6482@gmail.com>
2023-08-26 11:29:04 -0700

    Feat: Update :rocket:

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status')
 def status():
     """Return the status of our api"""
     dic = {"status": "OK"}
