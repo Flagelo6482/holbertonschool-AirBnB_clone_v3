740ff696ad09ff77e4b78607aa11df49032ab4fe
Flagelo6482 <frankln6482@gmail.com>
2023-08-27 09:05:28 -0700

    Feat: Adding function 404 :rocket:

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Flask web application"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def close(exception=None):
     """Method close"""
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    """Return json with 404 error"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
